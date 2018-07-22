b9def5105025bacbb62c977da2b59bc5c296ea8a
mehaotian <490272692@qq.com>
2018-07-22 21:39:07 +0800

    修改配置

--- pages/canvas/canvas.js
+++ pages/canvas/canvas.js
@@ -1,13 +1,19 @@
 Page({
   data: {
     value: 0,
-    styles: {
+    styles: [{
       line: '#dbdbdb',
       bginner: '#fbfbfb',
       bgoutside: '#dbdbdb',
       lineSelect: '#52b8f5',
       font: '#404040'
-    }
+    }, {
+        line: '#dbdbdb',
+        bginner: '#fbfbfb',
+        bgoutside: '#dbdbdb',
+        lineSelect: '#52b8f5',
+        font: '#404040'
+    }]
   },
   bindvalue: function (e) {
     // console.log(e)
